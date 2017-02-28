f1e89ee96b9291cde692dde0b1b0a0610deb10b1
1600558 <a@a.com>
2017-02-28 16:17:23 +0000

    index

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
 <body>
 <p>
     <?php
+
     echo "Hello World!";
+
     echo "Hello"." "."World"."!";
-    echo "5*7";
+
+    echo 5*7;
+
+    $name="Frodo Baggins";
+    $age="111";
+    echo "My name is".$name. "am I am".$age;
     ?>
 </p>
 </body>
