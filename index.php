de9c9b23754a7cdeb80a2334d39e93fa804dfdf3
1600558 <a@a.com>
2017-02-28 16:25:07 +0000

    index

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 
     echo "Hello World!";
 
-    echo "Hello"." "."World"."!";
+    echo "Hello" . " " . "World" . "!";
 
-    echo "5*7";
+    echo 5 * 7;
 
     $name = "Frodo Baggins";
     $age = "111";
-    echo "My name is".$name."am I am".$age;
+    echo "My name is" . $name . "am I am" . $age;
 
     echo "I get printed!";
     //I don't I'm a comment.
