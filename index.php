b55cb6e58c0676e258cfa9a099e3fd97501dcd74
1600558 <a@a.com>
2017-02-28 18:09:22 +0000

    last

--- index.php
+++ index.php
@@ -74,7 +74,15 @@ echo "My name is"." ". " " . $hobbit . "and I am"." ". $age;
             echo "All the hobbits have gone home";
     }
     ?>
+<p>
+    <?php
+    $myArray=array("do","re","mi"); //declare the array
 
+    echo $myArray[0];//outputs "do"
+    $myArray[1]= "la";//modifies position 1 (re)
+    echo $myArray[1];//outputs "la"
+    unset($myArray[2]);//removes the array in position 2
+    ?>
 </p>
 </body>
 </html>
