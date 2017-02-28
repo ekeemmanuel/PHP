a0155f68caade31c1c7413c6a685f901d3e49cf6
1600558 <a@a.com>
2017-02-28 15:46:26 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1600558
+ * Date: 28/02/2017
+ * Time: 14:06
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    ?>
+</p>
+</body>
+</html>
